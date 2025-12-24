4a203019bda91d18d82bf4605d42b78db7012509
francesco-mauro <francesco.mauro180@gmail.com>
2025-12-24 21:05:23 +0100

    feat: stable new game logic

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -23,7 +23,11 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
         <ol id="game-board">
             {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
-                    {row.map((playerSymbol, colIndex) => <li key={colIndex}><button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button></li>)}
+                    {row.map((playerSymbol, colIndex) =>
+                    (<li key={colIndex}>
+                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                    </li>
+                    ))}
                 </ol>
             </li>)}
         </ol>
